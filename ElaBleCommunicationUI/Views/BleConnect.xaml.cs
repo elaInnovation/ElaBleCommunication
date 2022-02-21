52836ade5d764ace6aeab6f95430fc4ad7fd3dbd
Stephane BONNET <stephane.bonnet@elainnovation.com>
2022-02-21 15:51:47 +0100

    Fix error handling

--- ElaBleCommunicationUI/Views/BleConnect.xaml.cs
+++ ElaBleCommunicationUI/Views/BleConnect.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using ElaSoftwareCommon.Error;
 
 namespace ElaBleCommunicationUI.Views
 {
@@ -69,9 +70,9 @@ namespace ElaBleCommunicationUI.Views
             try
             {
                 uint errorSend = await bleconnection.SendCommandAsync(tbMacCommandValue.Text, this.tbMacPasswordValue.Text, this.tbMacargsValue.Text);
-                if (errorSend != ErrorHandler.ERR_OK)
+                if (errorSend != ErrorServiceHandlerBase.ERR_OK)
                 {
-                    writeConsole($"Send function return error code : {errorSend}");
+                    writeConsole($"Send function return error code : {errorSend} ({ErrorServiceHandlerBase.getErrorMessage(errorSend)})");
                 }
                 else
                 {
@@ -90,9 +91,9 @@ namespace ElaBleCommunicationUI.Views
             try
             {
                 uint errorConnect = await bleconnection.ConnectDeviceAsync(this.tbMacAddressValue.Text);
-                if(errorConnect != ErrorHandler.ERR_OK)
+                if(errorConnect != ErrorServiceHandlerBase.ERR_OK)
                 {
-                    writeConsole($"Connect function return error code : {errorConnect}");
+                    writeConsole($"Connect function return error code : {errorConnect} ({ErrorServiceHandlerBase.getErrorMessage(errorConnect)})");
                 }
                 else
                 {
@@ -110,9 +111,9 @@ namespace ElaBleCommunicationUI.Views
             try
             {
                 uint error = bleconnection.DisconnectDevice();
-                if (error != ErrorHandler.ERR_OK)
+                if (error != ErrorServiceHandlerBase.ERR_OK)
                 {
-                    writeConsole($"Disconnect function return error code : {error}");
+                    writeConsole($"Disconnect function return error code : {error} ({ErrorServiceHandlerBase.getErrorMessage(error)})");
                 }
                 else
                 {
